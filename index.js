237229d15af9a056e8aa5d2031218f2cbd5eb07e
juanjoseholguin <juanjoseholguin128@gmail.com>
2024-10-22 11:56:18 -0500

    listo

---
+++ index.js
@@ -0,0 +1,75 @@
+let tasks = [];
+
+function addTask() {
+    const newTaskInput = document.getElementById('newTask');
+    const taskName = newTaskInput.value.trim();
+
+    if (taskName !== '') {
+        tasks.push({ name: taskName, status: 'Pending' });
+        newTaskInput.value = ''; // Limpiar el input después de añadir la tarea
+        updateInterface();
+    }
+}
+
+function updateInterface() {
+    const pendingList = document.getElementById('pendingList');
+    const inProgressList = document.getElementById('inProgressList');
+    const completedList = document.getElementById('completedList');
+
+    pendingList.innerHTML = '';
+    inProgressList.innerHTML = '';
+    completedList.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+        const taskElement = document.createElement('div');
+        taskElement.textContent = task.name;
+        taskElement.classList.add('task');
+
+        if (task.status === 'Pending') {
+            const arrowRight = document.createElement('button');
+            arrowRight.textContent = '→';
+            arrowRight.onclick = function() {
+                moveTaskToInProgress(index);
+            };
+            taskElement.appendChild(arrowRight);
+            pendingList.appendChild(taskElement);
+        } else if (task.status === 'InProgress') {
+            const arrowLeft = document.createElement('button');
+            arrowLeft.textContent = '←';
+            arrowLeft.onclick = function() {
+                moveTaskBackToPending(index);
+            };
+
+            const arrowRight = document.createElement('button');
+            arrowRight.textContent = '→';
+            arrowRight.onclick = function() {
+                moveTaskToCompleted(index);
+            };
+
+            const arrowsContainer = document.createElement('div');
+            arrowsContainer.classList.add('in-progress-arrows');
+            arrowsContainer.appendChild(arrowLeft);
+            arrowsContainer.appendChild(arrowRight);
+
+            taskElement.appendChild(arrowsContainer);
+            inProgressList.appendChild(taskElement);
+        } else if (task.status === 'Completed') {
+            completedList.appendChild(taskElement);
+        }
+    });
+}
+
+function moveTaskToInProgress(index) {
+    tasks[index].status = 'InProgress';
+    updateInterface();
+}
+
+function moveTaskBackToPending(index) {
+    tasks[index].status = 'Pending';
+    updateInterface();
+}
+
+function moveTaskToCompleted(index) {
+    tasks[index].status = 'Completed';
+    updateInterface();
+}
